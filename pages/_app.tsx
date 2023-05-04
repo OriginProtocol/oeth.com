6ceb44911f1eb014c7a5a25a07ee5f23a59ad5f6
Jonathan Snow <jon@sidepart.com>
2023-05-03 17:17:48 -0700

    Add GTM tracking code.
    Signed-off-by: Jonathan Snow <jon@sidepart.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import "@originprotocol/origin-storybook/lib/styles.css";
 import "../styles/globals.css";
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 import { AppProps } from "next/app";
+import Script from "next/script";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { GTM_ID, pageview, event } from "../utils/gtm";
 
 const defaultQueryFn = async ({ queryKey }) => {
   return await fetch(queryKey).then((res) => res.json());
@@ -17,10 +20,33 @@ const queryClient = new QueryClient({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on("routeChangeComplete", pageview);
+    return () => {
+      router.events.off("routeChangeComplete", pageview);
+    };
+  }, [router.events]);
+
   return (
     <QueryClientProvider client={queryClient}>
       {/* @ts-ignore */}
-      <Component {...pageProps} />
+      <>
+        <Script
+          id="gtag-base"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', '${GTM_ID}');
+            `,
+          }}
+        />
+        <Component {...pageProps} />
+      </>
     </QueryClientProvider>
   );
 }
---
+++ utils/gtm.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-undef
+export const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID
+
+export const pageview = () => {
+  window?.dataLayer?.push({
+    event: 'pageview',
+    pageUrl: window.location.href,
+    pageTitle: document.title,
+  })
+}
+
+export const event = (eventData) => {
+  window?.dataLayer?.push(eventData)
+}

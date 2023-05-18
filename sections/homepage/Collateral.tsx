af1e9e6a0ed2efbaafd4b74cfd7166097f7bbb84
Hrik Bhowal <hrik.bhowal@gmail.com>
2023-05-18 11:14:12 -0400

    remove log

--- sections/homepage/Collateral.tsx
+++ sections/homepage/Collateral.tsx
@@ -224,11 +224,6 @@ const Collateral = ({ collateral, strategies }: CollateralProps) => {
                                   className="flex flex-row space-x-1"
                                 >
                                   <Typography.Body3 className="text-[12px] leading-[19px] text-subheading">
-                                    {console.log(
-                                      token,
-                                      strategy,
-                                      strategies[strategy]
-                                    )}
                                     {`Ξ${rounded(
                                       strategies[strategy].holdings[
                                         token.toUpperCase()

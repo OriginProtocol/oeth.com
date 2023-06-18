5c9de4e9813bf796833cf48986b6be18bb8c10dc
Hrik Bhowal <hrik.bhowal@gmail.com>
2023-06-17 20:48:07 -0700

    numberless collateral component

---
+++ constants/collaterals.ts
@@ -0,0 +1,29 @@
+const collaterals = {
+  eth: {
+    img: "eth.svg",
+    tokenName: "Ether",
+    symbol: "ETH",
+  },
+  weth: {
+    img: "weth-icon.svg",
+    tokenName: "Wrapped Ether",
+    symbol: "WETH",
+  },
+  steth: {
+    img: "lido-icon.svg",
+    tokenName: "Lido Staked ETH",
+    symbol: "stETH",
+  },
+  reth: {
+    img: "reth-icon.svg",
+    tokenName: "Rocket Pool ETH",
+    symbol: "rETH",
+  },
+  frxeth: {
+    img: "frax-icon.svg",
+    tokenName: "Frax Ether",
+    symbol: "frxETH",
+  },
+};
+
+export default collaterals;

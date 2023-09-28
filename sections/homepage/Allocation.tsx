a49b9fcc3d7248b2e2719ee1364054a8141c5556
Nick Poulden <nick@poulden.com>
2023-09-28 15:16:06 -0600

    MUI overkill 😅

--- sections/homepage/Allocation.tsx
+++ sections/homepage/Allocation.tsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import { Typography } from '@originprotocol/origin-storybook'
-import LinearProgress from '@mui/material/LinearProgress'
-import { ThemeProvider } from '@mui/material/styles'
 import { formatCurrency, assetRootPath, camelifyLsd } from '../../utils'
-import { theme, strategyMapping, protocolMapping } from '../../constants'
+import { strategyMapping, protocolMapping } from '../../constants'
 import { groupBy } from 'lodash'
 import { Strategies } from '../../types'
 import { GradientButton } from '../../components'
@@ -99,164 +97,154 @@ const Allocation = ({ strategies }: AllocationProps) => {
                 <div className="text-[14px] md:text-[16px]">Allocation</div>
               </Typography.H7>
               <div className="flex flex-col px-[16px] md:px-10 pt-2 pb-[10px] md:pt-3 md:pb-8">
-                <ThemeProvider theme={theme}>
-                  <div className="flex flex-col justify-between">
-                    {loaded &&
-                      protocolsSorted?.map((protocol, i) => {
-                        if (protocol.name == 'undefined') return
-                        return (
-                          <div
-                            className="strategy rounded-xl border-2 p-[16px] md:p-8 my-[6px] md:my-[8px]"
-                            key={i}
-                            onClick={(e) => {
-                              e.preventDefault()
-                              setOpen({
-                                ...open,
-                                [protocol.name]: !open[protocol.name]
-                              })
-                            }}
-                          >
-                            <div>
-                              <div className="flex flex-row justify-between">
-                                <div
-                                  className={`relative ${
-                                    protocol.name !== 'Vault'
-                                      ? 'w-1/3 lg:w-1/4'
-                                      : 'w-fit'
-                                  }`}
-                                >
-                                  {protocol.name !== 'Vault' ? (
-                                    <Image
-                                      src={
-                                        protocolMapping[protocol.name]?.image
-                                      }
-                                      fill
-                                      sizes="(max-width: 768px) 64px, 128px"
-                                      style={{
-                                        objectFit: 'contain',
-                                        objectPosition: '0%'
-                                      }}
-                                      alt={protocol.name}
-                                    />
-                                  ) : (
-                                    <Typography.Body>
-                                      Unallocated
-                                    </Typography.Body>
-                                  )}
-                                </div>
-                                <div>
-                                  <Typography.H7
-                                    className="inline items-center text-[12px] md:text-[24px] text-subheading"
-                                    style={{ fontWeight: 400 }}
-                                  >{`${formatCurrency(
-                                    protocol.total,
-                                    2
-                                  )}`}</Typography.H7>
-                                  <Typography.H7
-                                    className="inline pl-[8px] text-[12px] md:text-[24px]"
-                                    style={{ fontWeight: 700 }}
-                                  >{`(${formatCurrency(
-                                    (protocol.total / total) * 100,
-                                    2
-                                  )}%)`}</Typography.H7>
-                                </div>
-                              </div>
-                              <LinearProgress
-                                variant="determinate"
-                                value={(protocol.total / total) * 100}
-                                // @ts-ignore
-                                color={protocol.name}
-                                sx={{
-                                  bgcolor: '#141519',
-                                  borderRadius: 10,
-                                  height: 4
-                                }}
-                                className="mt-5"
-                              />
-                              <Typography.Caption2
-                                className={`flex flex-row mt-4 md:hidden text-left space-x-1.5 text-subheading font-medium ${
-                                  open[protocol.name] ? 'hidden' : ''
+                <div className="flex flex-col justify-between">
+                  {loaded &&
+                    protocolsSorted?.map((protocol, i) => {
+                      if (protocol.name == 'undefined') return
+                      const pct = (protocol.total / total) * 100
+                      const width = `${pct.toFixed(2)}%`
+                      const backgroundColor =
+                        protocolMapping[protocol.name].color
+                      return (
+                        <div
+                          className="strategy rounded-xl border-2 p-[16px] md:p-8 my-[6px] md:my-[8px]"
+                          key={i}
+                          onClick={(e) => {
+                            e.preventDefault()
+                            setOpen({
+                              ...open,
+                              [protocol.name]: !open[protocol.name]
+                            })
+                          }}
+                        >
+                          <div>
+                            <div className="flex flex-row justify-between">
+                              <div
+                                className={`relative ${
+                                  protocol.name !== 'Vault'
+                                    ? 'w-1/3 lg:w-1/4'
+                                    : 'w-fit'
                                 }`}
                               >
-                                <div>More info</div>
+                                {protocol.name !== 'Vault' ? (
+                                  <Image
+                                    src={protocolMapping[protocol.name]?.image}
+                                    fill
+                                    sizes="(max-width: 768px) 64px, 128px"
+                                    style={{
+                                      objectFit: 'contain',
+                                      objectPosition: '0%'
+                                    }}
+                                    alt={protocol.name}
+                                  />
+                                ) : (
+                                  <Typography.Body>Unallocated</Typography.Body>
+                                )}
+                              </div>
+                              <div>
+                                <Typography.H7
+                                  className="inline items-center text-[12px] md:text-[24px] text-subheading"
+                                  style={{ fontWeight: 400 }}
+                                >{`${formatCurrency(
+                                  protocol.total,
+                                  2
+                                )}`}</Typography.H7>
+                                <Typography.H7
+                                  className="inline pl-[8px] text-[12px] md:text-[24px]"
+                                  style={{ fontWeight: 700 }}
+                                >{`(${formatCurrency(
+                                  (protocol.total / total) * 100,
+                                  2
+                                )}%)`}</Typography.H7>
+                              </div>
+                            </div>
+                            <div className="mt-5 bg-origin-bg-black">
+                              <div
+                                className={`rounded-full h-1`}
+                                style={{ backgroundColor, width }}
+                              />
+                            </div>
+                            <Typography.Caption2
+                              className={`flex flex-row mt-4 md:hidden text-left space-x-1.5 text-subheading font-medium ${
+                                open[protocol.name] ? 'hidden' : ''
+                              }`}
+                            >
+                              <div>More info</div>
+                              <Image
+                                src={assetRootPath(`/images/arrow-down.svg`)}
+                                width="10"
+                                height="6"
+                                alt="arrow"
+                              />
+                            </Typography.Caption2>
+                            <div
+                              className={`${
+                                open[protocol.name] ? '' : 'hidden md:block'
+                              }`}
+                            >
+                              <div className="flex flex-col xl:flex-row mt-[22px] xl:mt-3 flex-wrap space-y-2 xl:space-y-0">
+                                {protocol.strategies
+                                  .filter(
+                                    (s) =>
+                                      protocol.name !== 'Vault' || s.total !== 0
+                                  )
+                                  .map((strategy, i) => {
+                                    return (
+                                      <div
+                                        className="flex flex-row justify-between xl:mr-10 xl:pt-2.5"
+                                        key={i}
+                                      >
+                                        <div className="flex flex-row items-center">
+                                          <div
+                                            className={`relative ${
+                                              strategy?.protocol === 'Convex' ||
+                                              strategy?.protocol === 'Aura'
+                                                ? 'w-[48px] h-[24px]'
+                                                : 'w-[24px] h-[24px]'
+                                            }`}
+                                          >
+                                            <Image
+                                              src={strategy?.icon}
+                                              alt={strategy.name}
+                                              fill
+                                            />
+                                          </div>
+                                          <Typography.Body3 className="pl-[12px] pr-[16px] font-light text-[12px] md:text-[16px]">
+                                            {protocol.name === 'Vault'
+                                              ? camelifyLsd(strategy.name)
+                                              : strategy.name}
+                                          </Typography.Body3>
+                                        </div>
+                                        <Typography.Body3 className="text-subheading font-light text-[12px] md:text-[16px]">
+                                          {`${formatCurrency(
+                                            protocol.total
+                                              ? (strategy.total / total) * 100
+                                              : 0,
+                                            2
+                                          )}%`}
+                                        </Typography.Body3>
+                                      </div>
+                                    )
+                                  })}
+                              </div>
+                              <Typography.Body3 className="mt-4 text-subheading text-left text-[12px] md:text-[14px] leading-[23px]">
+                                {protocolMapping[protocol.name]?.description}
+                              </Typography.Body3>
+                              <Typography.Body3 className="flex flex-row mt-4 md:hidden text-left space-x-1.5 text-subheading text-[12px] font-medium">
+                                <div>Less info</div>
                                 <Image
-                                  src={assetRootPath(`/images/arrow-down.svg`)}
+                                  src={assetRootPath(`/images/arrow-up.svg`)}
                                   width="10"
                                   height="6"
                                   alt="arrow"
                                 />
-                              </Typography.Caption2>
-                              <div
-                                className={`${
-                                  open[protocol.name] ? '' : 'hidden md:block'
-                                }`}
-                              >
-                                <div className="flex flex-col xl:flex-row mt-[22px] xl:mt-3 flex-wrap space-y-2 xl:space-y-0">
-                                  {protocol.strategies
-                                    .filter(
-                                      (s) =>
-                                        protocol.name !== 'Vault' ||
-                                        s.total !== 0
-                                    )
-                                    .map((strategy, i) => {
-                                      return (
-                                        <div
-                                          className="flex flex-row justify-between xl:mr-10 xl:pt-2.5"
-                                          key={i}
-                                        >
-                                          <div className="flex flex-row items-center">
-                                            <div
-                                              className={`relative ${
-                                                strategy?.protocol ===
-                                                  'Convex' ||
-                                                strategy?.protocol === 'Aura'
-                                                  ? 'w-[48px] h-[24px]'
-                                                  : 'w-[24px] h-[24px]'
-                                              }`}
-                                            >
-                                              <Image
-                                                src={strategy?.icon}
-                                                alt={strategy.name}
-                                                fill
-                                              />
-                                            </div>
-                                            <Typography.Body3 className="pl-[12px] pr-[16px] font-light text-[12px] md:text-[16px]">
-                                              {protocol.name === 'Vault'
-                                                ? camelifyLsd(strategy.name)
-                                                : strategy.name}
-                                            </Typography.Body3>
-                                          </div>
-                                          <Typography.Body3 className="text-subheading font-light text-[12px] md:text-[16px]">
-                                            {`${formatCurrency(
-                                              protocol.total
-                                                ? (strategy.total / total) * 100
-                                                : 0,
-                                              2
-                                            )}%`}
-                                          </Typography.Body3>
-                                        </div>
-                                      )
-                                    })}
-                                </div>
-                                <Typography.Body3 className="mt-4 text-subheading text-left text-[12px] md:text-[14px] leading-[23px]">
-                                  {protocolMapping[protocol.name]?.description}
-                                </Typography.Body3>
-                                <Typography.Body3 className="flex flex-row mt-4 md:hidden text-left space-x-1.5 text-subheading text-[12px] font-medium">
-                                  <div>Less info</div>
-                                  <Image
-                                    src={assetRootPath(`/images/arrow-up.svg`)}
-                                    width="10"
-                                    height="6"
-                                    alt="arrow"
-                                  />
-                                </Typography.Body3>
-                              </div>
+                              </Typography.Body3>
                             </div>
                           </div>
-                        )
-                      })}
-                  </div>
-                </ThemeProvider>
+                        </div>
+                      )
+                    })}
+                </div>
               </div>
             </div>
           </div>

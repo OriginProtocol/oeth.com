f04f817cafd63491c989b56e11cbec7a21a9bd41
Nick Poulden <nick@originprotocol.com>
2023-10-06 15:03:58 -0600

    Query fix

--- queries/useApyHistoryQuery.ts
+++ queries/useApyHistoryQuery.ts
@@ -4,7 +4,7 @@ import { QUERY_KEYS } from "../constants";
 import { fetchApyHistory } from "../utils";
 
 const useApyHistoryQuery = (apyHistory, options = {}) => {
-  return useQuery(QUERY_KEYS.ApyHistory(365), () => fetchApyHistory, {
+  return useQuery(QUERY_KEYS.ApyHistory(365), () => fetchApyHistory(), {
     initialData: apyHistory,
     refetchOnWindowFocus: false,
     keepPreviousData: true,
